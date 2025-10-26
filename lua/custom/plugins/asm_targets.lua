365988e000c8b7f9bc5093866a14e006a1183004
Ibrahim Iqbal <ibrahim@ebs.net.pk>
2025-10-26 22:29:12 +0100

    asm target bullshit

--- lua/custom/plugins/asm_targets.lua
+++ lua/custom/plugins/asm_targets.lua
@@ -16,6 +16,17 @@ if vim.fn.executable 'asm-lsp' == 1 then
   }
 end
 
+-- Add clangd for C function validation in assembly files
+if vim.fn.executable 'clangd' == 1 then
+  lspconfig.clangd.setup {
+    cmd = { 'clangd' },
+    filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda', 'asm', 'armasm' },
+    root_dir = lspconfig.util.root_pattern('.clangd', '.clang-tidy', '.clang-format', 'compile_commands.json', '.asm-lsp.toml', '.git'),
+    -- Optional: customize capabilities if needed
+    capabilities = vim.lsp.protocol.make_client_capabilities(),
+  }
+end
+
 -- Set filetype for .S and .s files
 vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
   pattern = { '*.S', '*.s' },
@@ -24,11 +35,18 @@ vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
   end,
 })
 
--- Set comment string for asm files
+-- safer FileType handler: use opt_local and idempotent syntax commands
 vim.api.nvim_create_autocmd('FileType', {
   pattern = { 'asm', 'armasm' },
   callback = function()
-    vim.bo.commentstring = '@ %s'
+    -- set buffer-local commentstring using the proper API
+    vim.opt_local.commentstring = '@ %s'
+
+    -- don't re-run syntax definitions for this buffer
+    if vim.b.asm_comment_ext_added then
+      return
+    end
+    vim.b.asm_comment_ext_added = true
   end,
 })
 
