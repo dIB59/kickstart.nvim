aeda585344a3bca9ae5c475ecafa058baa675f00
Ibrahim Iqbal <ibrahim@ebs.net.pk>
2026-01-01 07:16:45 +0100

    add hardtime

---
+++ lua/custom/plugins/hardtime.lua
@@ -0,0 +1,15 @@
+return {
+  'm4xshen/hardtime.nvim',
+  dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
+  opts = {
+    max_count = 2, -- Maximum count of repeated keys
+    disable_mouse = true,
+    restriction_mode = 'hint', -- or 'block'
+    disabled_keys = {
+      ['<Up>'] = {},
+      ['<Down>'] = {},
+      ['<Left>'] = {},
+      ['<Right>'] = {},
+    },
+  },
+}
