ed8de095093e765cec2a39e3bab6741321869575
Ibrahim Iqbal <ibrahim@ebs.net.pk>
2026-01-03 17:06:36 +0100

    add a greeter

---
+++ lua/custom/plugins/greeter.lua
@@ -0,0 +1,29 @@
+return {
+  'folke/snacks.nvim',
+  priority = 1000,
+  lazy = false,
+  opts = {
+    dashboard = {
+      enabled = true,
+      sections = {
+        { section = 'header' },
+        { section = 'keys', gap = 1, padding = 1 },
+        { title = 'Recent Projects', section = 'projects', padding = 1 },
+        { section = 'startup' },
+      },
+      preset = {
+        -- Define your specific buttons here
+        keys = {
+          -- Using Capital letters or unique symbols prevents motion conflicts
+          { icon = '󰛄 ', key = 'L', desc = 'LeetCode', action = ':Leet' },
+          { icon = '󰌌 ', key = 'T', desc = 'Typr', action = ':Typr' },
+          { icon = '󰒲 ', key = 'S', desc = 'Status', action = ':Lazy' },
+          { icon = '󱎫 ', key = 'T', desc = 'Typr Stats', action = ':TyprStats' },
+          { icon = ' ', key = 'Q', desc = 'Quit', action = ':qa' },
+          -- Use 'p' for project search since it's not a vertical motion
+          { icon = ' ', key = 'f', desc = 'Find Project', action = ':lua Snacks.picker.projects()' },
+        },
+      },
+    },
+  },
+}
