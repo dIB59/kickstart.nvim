1d64cfd3ff8cdc8df89f761ee4580388e45803a0
Ibrahim Iqbal <ibrahim@ebs.net.pk>
2026-01-03 21:21:14 +0100

    change key

--- lua/custom/plugins/greeter.lua
+++ lua/custom/plugins/greeter.lua
@@ -20,7 +20,7 @@ return {
           { icon = '󰒲 ', key = 'S', desc = 'Status', action = ':Lazy' },
           { icon = '󱎫 ', key = 'm', desc = 'Typr Stats', action = ':TyprStats' },
           { icon = '󰀚 ', key = 'P', desc = 'Triforce Profile', action = ':Triforce profile' }, -- Use 'p' for project search since it's not a vertical motion
-          { icon = ' ', key = 'f', desc = 'Find Project', action = ':lua Snacks.picker.projects()' },
+          { icon = ' ', key = 'F', desc = 'Find Project', action = ':lua Snacks.picker.projects()' },
           { icon = ' ', key = 'Q', desc = 'Quit', action = ':qa' },
         },
       },
